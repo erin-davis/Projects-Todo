feeb7c18c9d4b9b5264185039a8b2db39ea895ee
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-06-21 22:43:13 -0500

    started form and form functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TodoForm from "./components/TodoForm.js";
 
 function App() {
+
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "This is a tester note",
+      noteBody:
+        "And here is a larger portion of the note. I really only want to use this note to make sure that every portion of this will work well!!"
+    }
+  ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is a Todo List!</h1>
+      <TodoForm />
     </div>
   );
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+
+export default function TodoForm(props){
+
+  const [note, setNote] = useState({
+    title: "",
+    noteBody: ""
+  });
+
+  const handleChanges = e =>{
+    setNote({...note, [e.target.name]: e.target.value});
+    console.log(e.target.value);
+  };
+
+  const submitForm = e =>{
+    e.preventDefault();
+  }
+
+  //note: there should be a confirmation alert with the reset form since it'll reset the full form
+  const resetForm = e =>{
+    e.preventDefault()
+  }
+
+  return(
+    <div className="todo-form">
+      <form id="notes-form" className="notes-form" onSubmit={submitForm}>
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          type="text"
+          placeholder="Title"
+          name="title"
+          value={note.title}
+          onChange={handleChanges}
+         />
+        <label htmlFor="notes">Notes:</label>
+        <textarea
+          name="notes"
+          id="notes"
+          form="notes-form"
+          onChange={handleChanges}
+          >
+          Further detail here...
+          </textarea>
+         <input
+          type="checkbox"
+          id="check-save"
+          name="check-save"
+          value="Save"
+         />
+         <label htmlFor="check-save">Save?</label>
+         <button type="submit">Add Todo</button>
+         <button type="reset">Reset Page</button>
+      </form>
+    </div>
+  )
+}
